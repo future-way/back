256e135f820a5f4088f3253fc52a7e376831ee89
seungwon <swlee173@gmail.com>
2024-12-09 23:14:32 +0900

    feat: 예외 처리 및 로깅 기능 추가
    - CoreException, ErrorType, ErrorCode, ErrorResponse 클래스 추가
    - LoggingFilter 클래스 추가
    - GlobalExceptionHandler를 통한 글로벌 예외 처리 구현
    - 요청 및 응답 로깅 기능 포함

---
+++ src/main/java/com/team/futureway/common/exception/CoreException.java
@@ -0,0 +1,21 @@
+package com.team.futureway.common.exception;
+
+
+public class CoreException extends RuntimeException {
+    private final ErrorType errorType;
+    private final Object payload;
+
+    public CoreException(ErrorType errorType, Object payload) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+        this.payload = payload;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+}
