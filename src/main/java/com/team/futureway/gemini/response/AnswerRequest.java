0d167459967b9bf8863ee47eb4bdbabd1f2dfb8f
seungwon <swlee173@gmail.com>
2024-12-11 16:22:24 +0900

    refactor: AnswerRequest에서 of 메서드 제거

--- src/main/java/com/team/futureway/gemini/response/AnswerRequest.java
+++ src/main/java/com/team/futureway/gemini/response/AnswerRequest.java
@@ -1,10 +1,4 @@
 package com.team.futureway.gemini.response;
 
-import com.team.futureway.gemini.dto.QuestionDTO;
-
 public record AnswerRequest(Long aiConsultationHistoryId, Long userId, int questionNumber, String answer) {
-
-    public static AnswerRequest of(QuestionDTO dto) {
-        return new AnswerRequest(dto.getAiConsultationHistoryId(), dto.getUserId(), dto.getQuestionNumber(), dto.getAnswer());
-    }
 }
