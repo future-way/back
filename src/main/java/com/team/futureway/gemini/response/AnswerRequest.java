f1b071128264ed71b6b55b6034dc5f7b84990005
seungwon <swlee173@gmail.com>
2024-12-12 16:50:40 +0900

    refactor: AnswerRequest에서 questionNumber 필드 제거

--- src/main/java/com/team/futureway/gemini/response/AnswerRequest.java
+++ src/main/java/com/team/futureway/gemini/response/AnswerRequest.java
@@ -1,4 +1,4 @@
 package com.team.futureway.gemini.response;
 
-public record AnswerRequest(Long aiConsultationHistoryId, Long userId, int questionNumber, String answer) {
+public record AnswerRequest(Long aiConsultationHistoryId, Long userId, String answer) {
 }
